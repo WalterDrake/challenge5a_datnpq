a68f683cecb1885d43361c3c52b6bd70018f8375
Walter Drake <dat123mall@gmail.com>
2025-03-29 02:17:26 +0700

    ok

---
+++ moodle.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 28, 2025 at 08:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moodle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `author` varchar(30) NOT NULL,
+  `date` date NOT NULL,
+  `location` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`location`)),
+  `submit` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`submit`)),
+  `assignment_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `sender` varchar(30) NOT NULL,
+  `receiver` varchar(30) NOT NULL,
+  `note` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`note`)),
+  `note_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` text NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `numbers` int(11) NOT NULL,
+  `user_id` text NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `avatar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `fullname`, `email`, `numbers`, `user_id`, `role`, `avatar`) VALUES
+(1, 'admin', '$2y$10$QVfllaw8kuNouSJ4R9EXGeOYe5zOZkLkCiHAnAkPbXado59.NWaeG', 'admin', 'admin@gmail.com', 123456789, '67e6f12993290', 'Administrator', ''),
+(2, 'teacher1', '$2y$10$SeslPwoFbRccykgOtZTU6espqjF4ul949sdvpGJKH5RZwDK2J90zG', 'teacherOne', 'teacher1@gmail.com', 2147483647, '67e6f2501c130', 'Teacher', ''),
+(3, 'teacher2', '$2y$10$nA86ROLc4bKUQ2q/xFrHZuxCn.OvaT9Rsd6rO8VcHpXBpdpaQ7jHS', 'teacherTwo', 'teacher2@gmail.com', 2147483647, '67e6f2811616f', 'Teacher', ''),
+(4, 'student1', '$2y$10$R/J2Ps5OLcVHTenWAfUj.eGPc009tkRIMedv.q3srb3Fvy1FYlpLO', 'studentOne', 'student1@gmail.com', 43242344, '67e6f2a666e0a', 'Student', ''),
+(5, 'student2', '$2y$10$oufoETvQxDSMHjvcqjALNO2pIILbf1D4yoBICK79nnpNDqGToRuRq', 'studentTwo', 'student2@gmail.com', 43242344, '67e6f2b529566', 'Student', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author` (`author`),
+  ADD KEY `assigment_id` (`assignment_id`(768));
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `note_id` (`note_id`(768)),
+  ADD KEY `receiver` (`receiver`),
+  ADD KEY `sender` (`sender`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`,`fullname`,`email`),
+  ADD KEY `user_id` (`user_id`(768));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD CONSTRAINT `assignments_ibfk_1` FOREIGN KEY (`author`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
